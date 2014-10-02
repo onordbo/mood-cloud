3bae2a07c9839c257cddc0e083c259acefe51087
Øyvind Nordbø <oyvinor@gmail.com>
2014-10-02 17:29:21 +0200

    Update sensorScript variables to reflect led instead of buzzer

--- Sensorscript.ino
+++ Sensorscript.ino
@@ -1,31 +1,32 @@
-int buzzerPin = 13;                // choose the pin for the Buzzer
+int ledPin = 13;             // choose the pin for the light
 int inputPin = 2;               // choose the input pin (for PIR sensor)
 int pirState = LOW;             // we start, assuming no motion detected
-int signal = 0;                    // variable for reading the pin status
- 
+int signal = 0;                 // variable for reading the pin status
+
 void setup() {
-  pinMode(buzzerPin, OUTPUT);      // declare Buzzer as output
-  pinMode(inputPin, INPUT);     // declare sensor as input 
+  pinMode(ledPin, OUTPUT);   // declare LED as output
+  pinMode(inputPin, INPUT);     // declare sensor as input
   Serial.begin(9600);
 }
- 
+
 void loop(){
   signal = digitalRead(inputPin);  // read input value
   if (signal == HIGH) {            // check if the input is HIGH
-    digitalWrite(buzzerPin, HIGH);  // turn Buzzer ON   
-    if (pirState == LOW) {     // we have just turned on   
-      Serial.println("Motion detected!");
+    digitalWrite(ledPin, HIGH);  // turn light ON
+    if (pirState == LOW) {     // we have just turned on
+      Serial.println("DICKS");
       // We only want to print on the output change, not state
       pirState = HIGH;
     }
   } else {
-    digitalWrite(buzzerPin, LOW); // turn Buzzer OFF
+
+    digitalWrite(ledPin, LOW); // turn light OFF
+
     if (pirState == HIGH){
       // we have just turned of
-      Serial.println("Motion ended!");
+      Serial.println("NO DICKS");
       // We only want to print on the output change, not state
       pirState = LOW;
-  }
+    }
   }
 }
-
